4dd14ba59f34297d2e3b0b6708f2eba6e2c5541f
zamzami16 <mohammad.yusuf.zamzami-2016@fst.unair.ac.id>
2022-05-16 18:34:38 +0700

    add share by email system

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 from django.views.generic import ListView
 
 from .forms import EmailPostForm
