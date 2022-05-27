2399cd751eb2bb139717c911bf1c7c6ab9c13225
zamzami16 <mohammad.yusuf.zamzami-2016@fst.unair.ac.id>
2022-05-27 10:00:02 +0700

    adding similarity post system

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from django.db.models import Count
 from django.views.generic import ListView
 
 from taggit.models import Tag
@@ -67,7 +68,7 @@ def post_list(request, tag_slug=None):
     return render(
         request,
         "blog/post/list.html",
-        {"page": page, "posts": posts, "tag": tag}
+        {"page": page, "posts": posts, "tag": tag},
     )
 
 
@@ -98,6 +99,15 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    # list of similar posts
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(
+        id=post.id
+    )
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
     return render(
         request,
         "blog/post/detail.html",
@@ -106,5 +116,6 @@ def post_detail(request, year, month, day, post):
             "comments": comments,
             "new_comment": new_comment,
             "comment_form": comment_form,
+            "similar_posts": similar_posts,
         },
     )
