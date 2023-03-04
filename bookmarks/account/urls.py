56cdf98c9cd6d5735d9c1769ff6f3cdff52014bf
Zami16 <yusufazzamzami@gmail.com>
2023-03-05 06:16:32 +0700

    bebersih

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -4,7 +4,18 @@ from . import views
 
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("", views.dashboard, name="dashboard"),
+    # change password url
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
 ]
